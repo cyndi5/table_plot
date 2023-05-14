e1d9af55405ea5a57c3eedc55924b8ea79443e54
Cyndi Cavanaugh <cyndicavanaugh5@gmail.com>
2023-05-14 18:26:56 -0500

    main

--- tkthing.py
+++ tkthing.py
@@ -1,21 +1,30 @@
+import argparse
 import tkinter as tk
 import tkinter.messagebox
+
 
 def handle_click(event):
     # Display a message showing which button was clicked.
     tk.messagebox.showinfo("Button Clicked", "You clicked the " + event + " button.")
 
 
-# Create a window.
-root = tk.Tk()
+def main():
+    # Create a window.
+    root = tk.Tk()
 
-# Create two buttons.
-button1 = tk.Button(root, text="Button 1", command=lambda: handle_click('1'))
-button2 = tk.Button(root, text="Button 2", command=lambda: handle_click('2'))
+    # Create two buttons.
+    button1 = tk.Button(root, text="Button 1", command=lambda: handle_click('1'))
+    button2 = tk.Button(root, text="Button 2", command=lambda: handle_click('2'))
 
-# Pack the buttons.
-button1.pack()
-button2.pack()
+    # Pack the buttons.
+    button1.pack()
+    button2.pack()
 
-# Start the main loop.
-root.mainloop()
+    # Start the main loop.
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    print("Running main()")
+    main()
+
